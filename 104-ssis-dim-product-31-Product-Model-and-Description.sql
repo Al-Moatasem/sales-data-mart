0b3dd3fcd262ec1f9b22cd72a90594889667a9b2
Al-Moatasem <47496345+Al-Moatasem@users.noreply.github.com>
2020-02-14 20:51:48 +0200

    Create 104-ssis-dim-product-31-Product-Model-and-Description.sql

---
+++ 104-ssis-dim-product-31-Product-Model-and-Description.sql
@@ -0,0 +1,11 @@
+SELECT
+	pm.ProductModelID,
+	pm.Name AS model_name,
+	pd.Description AS Product_description
+FROM [Production].[ProductModel] AS pm
+	LEFT JOIN production.ProductModelProductDescriptionCulture AS pdi 
+		ON pm.ProductModelID = pdi.ProductModelID
+	LEFT JOIN [Production].[ProductDescription] AS pd
+		ON pdi.ProductDescriptionID = pd.ProductDescriptionID
+UNION ALL
+SELECT null,null,null
