33f44a12a54ccf519eab1415c652b1658dba25c6
Al-Moatasem <47496345+Al-Moatasem@users.noreply.github.com>
2020-02-27 00:04:42 +0200

    Rename 105_ssis_fact_sales_02_full_load_02_sales_order_details.sql to 105_ssis_fact_sales_02_incremental_load_02_sales_order_details.sql

---
+++ 105_ssis_fact_sales_02_incremental_load_02_sales_order_details.sql
@@ -0,0 +1,15 @@
+﻿SELECT
+	sod.SalesOrderID AS SalesOrderID,
+	SalesOrderDetailID,
+	OrderQty,
+	ProductID,
+	UnitPrice,
+	UnitPriceDiscount,
+	LineTotal
+FROM sales.SalesOrderDetail AS sod
+INNER JOIN sales.SalesOrderHeader AS soh
+	ON sod.SalesOrderID = soh.SalesOrderID
+WHERE OnlineOrderFlag = 1
+AND soh.ModifiedDate >= ? -- user variable, last update date
+AND soh.ModifiedDate < ? -- system variabl, StartTime
+ORDER BY sales_order_id
